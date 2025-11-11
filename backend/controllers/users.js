1e83cd0f85abeab6caf3b7af3407f2c8328e3045
Ratchat1918 <160712854+Ratchat1918@users.noreply.github.com>
2025-11-11 15:05:41 +0200

    Add files via upload

---
+++ backend/controllers/users.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+  console.log('BODY:', request.body)
+  const { username, name, password } = request.body
+
+  if (!password || password.length < 3) {
+    return response.status(400).json({ error: 'password too short' })
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+  response.status(201).json(savedUser)
+})
+
+usersRouter.put
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
+usersRouter.get('/:id', async (request, response) => {
+  const user = await User.findById(request.params.id)
+  if (user) {
+    response.json(user)
+  } else {
+    response.status(404).end()
+  }
+})
+
+module.exports = usersRouter
---
+++ backend/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+require('./mongo')
+
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const boardsRouter = require('./controllers/boards')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
+
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+app.use('/api/boards', boardsRouter)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
